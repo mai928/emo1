bbd19c01c0f27be2a7f0e2bb16c521c8c371a0ab
maimohammed <maimohammed928@gmail.com>
2024-08-04 12:40:29 -0700

    footer position in responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,7 @@ import Partner from './Partner'
 const Footer = () => {
     return (
         <section>
-            {/* <div className='z-20 bg-opacity-50 bg-black  py-20 lg:py-0 absolute bottom-0  w-full  text-center'>
-                <Partner />
-                <div className='border-b-2 border-solid border-white w-full' />
-
-            </div> */}
-            <div className='z-10 bg-opacity-50 bg-black py-2 absolute bottom-0 left-0 w-full text-center'>
+            <div className='z-10 bg-opacity-50 bg-black py-4 absolute top-96 lg:bottom-0 left-0 w-full text-center'>
 
                 <p className='text-white'>designed EMOCO by cangrow @2024</p>
             </div>

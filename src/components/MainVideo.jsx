154a2dd5a4b9489e470dbb1aea12157a00f40e6e
maimohammed <maimohammed928@gmail.com>
2024-08-06 13:26:07 -0700

    video updated

--- src/components/MainVideo.jsx
+++ src/components/MainVideo.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const MainVideo = () => {
   return (
-    <div className='w-full h-screen z-0 absolute top-0'>
+    <div className='w-full h-screen z-0  relative'>
       <video
-        className=" w-full h-full object-cover"
+        className=" w-full h-full absolute top-0 left-0 object-cover"
         src='/video/main.mp4'
         autoPlay
         loop
@@ -13,7 +13,8 @@ const MainVideo = () => {
       <div className='absolute left-0 top-[50%]  bg-opacity-40 bg-footer_color w-full h-[10%] lg:h-[20%]' >
         <h1 className='font-shelley text-white text-3xl lg:text-9xl  2xl:text-9xl  left-[26%] bottom-[8%] absolute text-auto-complete'>leading the stream</h1>
       </div>
-    </div>)
+    </div>
+    )
 }
 
 export default MainVideo
